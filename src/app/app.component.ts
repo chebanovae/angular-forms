e03ed83737cc91bf9473f518425d708e3cdeaef0
Lisa <cheye03@ca.com>
2018-03-28 14:18:27 +0200

    Assignment: Forms - reactive approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  statusForm: FormGroup;
+  statuses = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit() {
+    this.statusForm = new FormGroup({
+      projectName: new FormControl(null, Validators.required, this.projectNameForbidden),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      projectStatus: new FormControl('Stable')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.statusForm);
+  }
+
+  projectNameForbidden(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      if (control.value === 'Test') {
+        resolve({ 'nameIsForbidden' : true});
+      } else {
+        resolve(null);
+      }
+    });
+  }
 }
