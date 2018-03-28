38fa3c5fad3e07df48b91b990d81b91ba8e223a4
Lisa <cheye03@ca.com>
2018-03-28 14:10:06 +0200

    Forms - TD approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('f') ngForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    /*this.ngForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    });*/
+    this.ngForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  onSubmit() {
+    this.user.username = this.ngForm.value.userData.username;
+    this.user.email = this.ngForm.value.userData.email;
+    this.user.secretQuestion = this.ngForm.value.secret;
+    this.user.answer = this.ngForm.value.questionAnswer;
+    this.user.gender = this.ngForm.value.gender;
+    this.submitted = true;
+
+    this.ngForm.reset();
+  }
+
 }
